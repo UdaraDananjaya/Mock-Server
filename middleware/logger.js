e2a7fbb739235c5f3c26387d12f9224480e09d47
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-05 20:56:20 +0530

    Adding Function to Log

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,18 +2,39 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res, next) => {
-  const specificDate = new Date(Date.now());
-  console.log(specificDate);
+  const startTime = process.hrtime(); // Start timing the request
+  const specificDate = new Date();
   const formattedDate = specificDate.toISOString().split('T')[0];
   const logFilePath = path.join(__dirname, '../logs', `${formattedDate}.log`);
-  // const logEntry = `${specificDate.toISOString()} - ${req.ip} -  ${req.method} - ${req.originalUrl} - ${JSON.stringify(req.body)} -  [${JSON.stringify(req.headers)}] \n`;
-  // const logEntry = `${specificDate.toISOString()} - ${req.ip} -  ${req.method} - ${req.originalUrl} - ${res.statusCode}  \n`;
-  const logEntry = `${req.ip} - ${formattedDate} - ${specificDate.toISOString()} - ${req.method} - ${req.originalUrl} - ${res.statusCode} - User-Agent: ${req.headers['user-agent']} - Referrer: ${req.headers['referer'] || 'N/A'}\n`;
 
-  fs.appendFile(logFilePath, logEntry, (err) => {
-    if (err) {
-      console.error("Logging error: ", err);
-    }
+  // Create a log entry function to format the log string
+  const createLogEntry = () => {
+    const duration = process.hrtime(startTime);
+    const responseTime = (duration[0] * 1e3 + duration[1] * 1e-6).toFixed(3); // Convert to milliseconds
+    const logEntry = [
+      `Date: ${specificDate.toISOString().split('T')[0]}`,
+      `Time: ${specificDate.toISOString().split('T')[1].split('.')[0]}`,
+      `IP: ${req.ip}`,
+      `Method: ${req.method}`,
+      `URL: ${req.originalUrl}`,
+      `Query: ${JSON.stringify(req.query)}`,
+      `Status: ${res.statusCode}`,
+      `User-Agent: ${req.headers['user-agent']}`,
+      `Referrer: ${req.headers['referer'] || 'N/A'}`,
+      `Response Time: ${responseTime} ms`
+    ].join('  |  ');
+
+    return `${logEntry}\n`;
+  };
+
+  // Listen for the response to log status code
+  res.on('finish', () => {
+    const logEntry = createLogEntry();
+    fs.appendFile(logFilePath, logEntry, (err) => {
+      if (err) {
+        console.error("Logging error: ", err);
+      }
+    });
   });
 
   next();
