b0bf0e2f89273c1386b129cdb9fc83a81a69a654
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-05 20:51:54 +0530

    Add More LogEntry

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,11 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res, next) => {
-  const logFilePath = path.join(__dirname, '../logs/application.log');
-  const logEntry = `${new Date().toISOString()} - ${req.ip} - ${req.method} - ${req.originalUrl} - ${JSON.stringify(req.body)} - [${JSON.stringify(req.headers)}]\n`;
+  const specificDate = new Date(Date.now());
+  console.log(specificDate);
+  const formattedDate = specificDate.toISOString().split('T')[0];
+  const logFilePath = path.join(__dirname, '../logs', `${formattedDate}.log`);
+  // const logEntry = `${specificDate.toISOString()} - ${req.ip} -  ${req.method} - ${req.originalUrl} - ${JSON.stringify(req.body)} -  [${JSON.stringify(req.headers)}] \n`;
+  // const logEntry = `${specificDate.toISOString()} - ${req.ip} -  ${req.method} - ${req.originalUrl} - ${res.statusCode}  \n`;
+  const logEntry = `${req.ip} - ${formattedDate} - ${specificDate.toISOString()} - ${req.method} - ${req.originalUrl} - ${res.statusCode} - User-Agent: ${req.headers['user-agent']} - Referrer: ${req.headers['referer'] || 'N/A'}\n`;
 
   fs.appendFile(logFilePath, logEntry, (err) => {
-    if (err) console.error("Logging error: ", err);
+    if (err) {
+      console.error("Logging error: ", err);
+    }
   });
 
   next();
